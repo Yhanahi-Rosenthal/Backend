4070410e8e52d3129669cc261190b27411400646
YhanahiRosenthal <yhanai@hotmail.com>
2023-04-27 21:37:10 -0300

    addProducts modified

--- challenge-02/testTwo.js
+++ challenge-02/testTwo.js
@@ -5,31 +5,36 @@ class ProductManager {
         this.path = path;
         this.products = [];
         this.counter = 0;
+        this.code = Math.random() + 1;
     }
 
     addProduct(title, description, price, thumbnail, stock) {
-        this.counter++;
 
         const productsArray = this.getProducts();
+        console.log('este es el console ' + productsArray[7])
 
         const existProduct = productsArray.find(product => product.title === title && product.description === description);
 
+        const lastProduct = productsArray[productsArray.length - 1];
+
         if (existProduct) {
-            existProduct.stock += stock;
+            existProduct.stock += 1;
         } else {
             const product = {
-                id: this.counter,
+                id: lastProduct ? lastProduct.id + 1 : 1,
                 title,
                 description,
                 price,
                 thumbnail,
-                stock
+                stock,
+                code: this.code
             };
+            
 
-            productsArray.push(product);
+            productsArray.push(product)
+            fs.writeFileSync(this.path, JSON.stringify(productsArray));
         }
 
-        fs.writeFileSync(this.path, JSON.stringify(productsArray));
     }
 
     getProducts() {
@@ -74,6 +79,11 @@ const manager = new ProductManager("Products.json");
 manager.addProduct('arroz', 'grano fino', 20, 'https://example.com/product1-a.jpg' , 12);
 manager.addProduct('salsa','salsa de tomate', 34, 'https://example.com/product2-b.jpg', 42);
 manager.addProduct('fideos','moña', 30, 'https://example.com/product3-c.jpg', 23);
+manager.addProduct('fiasdasddeos','moasdasdña', 32342340, 'https://example.com/product3-c.jpg', 2323423);
+manager.addProduct('papas fritas','frios', 42, 'https://example.com/product3-c.jpg', 200);
+manager.addProduct('coca-cola','comun', 120, 'https://example.com/product3-c.jpg', 250);
+manager.addProduct('sprite','comun', 230, 'https://example.com/product3-c.jpg', 280);
+manager.addProduct('fanta','comun', 230, 'https://example.com/product3-c.jpg', 280);
 
 const products = manager.getProducts();
 console.log(products);
