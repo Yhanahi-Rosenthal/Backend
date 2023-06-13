c51c50c730c73dab8758673bf5f17303a99ec002
YhanahiRosenthal <yhanai@hotmail.com>
2023-06-13 00:49:00 -0300

    carrito, socket, mongo, conexiones...

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,120 +1,93 @@
-import express from "express";
-import { ProductManager } from "../productManager.js"
+import express from 'express'
+import ProductService from '../services/products.service.js'
 
-const products = new ProductManager("./productos.json");
-export const productsRouter = express.Router();
+const productService = new ProductService()
+export const productRouter = express.Router()
 
-productsRouter.get("/", async (req, res, next) => {
-    try {
-        const data = await products.getProducts();
-        const limit = req.query.limit
-        const limitedProducts = limit ? data.slice(0, limit) : data;
-        res.status(200).json(limitedProducts)
-    } catch (err) {
-        if (err instanceof Error) {
-            res.status(500).json({ status: "error", msg: "Invalid input", data: {} })
-        } else {
-            res.status(500).json({ status: "error", msg: "Error in server", data: {} })
-        }
-    }
+productRouter.get('/', async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10
+    const page = parseInt(req.query.page) || 1
+    const sort = req.query.sort || ''
+    const query = req.query.query || ''
+
+    const result = await productService.getProducts({
+      limit,
+      page,
+      sort,
+      query
+    })
+
+    res.status(200).json({ status: 'success', ...result })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ status: 'error', message: 'error' })
+  }
+})
+productRouter.get('/:pid', async (req, res) => {
+  try {
+    const id = req.params.pid
+    const product = await productService.getProductById(id)
+    res.status(200).json({ status: 'success', payload: product })
+  } catch (error) {
+    res.status(500).json({ status: 'error', message: 'error' })
+  }
 })
 
-productsRouter.get("/:pid", async (req, res, next) => {
-    try {
-        const id = req.params.pid
-        const dataId = await products.getProductById(parseInt(id));
-        res.status(200).json(dataId)
-    } catch (err) {
-        if (err instanceof Error) {
-            res.status(500).json({ status: "error", msg: "Invalid input", data: {} })
-        } else {
-            res.status(500).json({ status: "error", msg: "Error in server", data: {} })
-        }
-    }
+productRouter.post('/', async (req, res) => {
+  const productData = req.body
+  try {
+    const product = await productService.createProduct(productData)
+    return res.status(201).json({
+      status: 'success',
+      msg: 'user created',
+      data: product
+    })
+  } catch (e) {
+    return res.status(500).json({
+      status: 'error',
+      msg: 'something went wrong :(',
+      data: {}
+    })
+  }
 })
 
-
-productsRouter.post("/", async (req, res, next) => {
-    try {
-        const data = await products.getProducts();
-        let newProduct = req.body;
-        let findproduct = (data.find((ele) => ele.code === newProduct.code))
-        if (findproduct) {
-            return res.status(400).json({
-                status: "error",
-                msg: "Product already exists"
-            })
-        }
-        const requiredField = ["title", "description", "code", "price", "stock", "category"]
-        const allFields = requiredField.every(prop => newProduct[prop]);
-        if (newProduct.id == undefined && allFields) {
-            newProduct =
-            {
-                ...newProduct,
-                id: data[data.length - 1].id + 1
-            }
-            await products.addProduct({ ...newProduct, status: true })
-            return res.status(200).json({
-                status: "success",
-                msg: "Product added successfully",
-                data: newProduct
-            })
-        } else {
-            res.status(400).json({
-                status: "error",
-                msg: "Invalid input"
-            })
-        }
-    } catch (err) {
-        if (err instanceof Error) {
-            res.status(500).json({ status: "error", msg: "Invalid input", data: {} })
-        } else {
-            res.status(500).json({ status: "error", msg: "Error in server", data: {} })
-        }
-    }
+productRouter.put('/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const productData = req.body
+    const updatedProduct = await productService.updateProduct(id, productData)
+    return res.status(201).json({
+      status: 'success',
+      msg: 'user uptaded',
+      data: updatedProduct
+    })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).json({
+      status: 'error',
+      msg: 'something went wrong :(',
+      data: {}
+    })
+  }
 })
 
-productsRouter.put("/:pid", async (req, res, next) => {
-    try {
-        const id = req.params.pid
-        const data = await products.getProducts()
-        let changeProduct = req.body;
-        await products.updateProduct(id, changeProduct);
-        return res.status(201).json({
-            status: "Success",
-            msg: "product updated",
-            data: changeProduct
-        })
-    } catch {
-        res.status(500).json({ status: "error", msg: "Invalid input", data: {} })
-    }
+productRouter.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    // eslint-disable-next-line no-unused-vars
+    const deletedProduct = await productService.deleteProduct(id)
+    return res.status(200).json({
+      status: 'success',
+      msg: 'user deleted',
+      data: {}
+    })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).json({
+      status: 'error',
+      msg: 'something went wrong :(',
+      data: {}
+    })
+  }
 })
-
-productsRouter.delete("/:pid", async (req, res, next) => {
-    try {
-        const id = req.params.pid
-        const data = await products.getProducts()
-        let findProduct = data.find((prod) => prod.id == id)
-        if (!findProduct) {
-            return res.status(400).json({
-                status: "error",
-                msg: "Product not found"
-            })
-        } else {
-            await products.deleteProduct(id);
-            return res.status(201).json({
-                status: "Success",
-                msg: "product deleted",
-                data: {}
-            })
-        }
-    } catch {
-        res.status(500).json({ status: "error", msg: "Invalid input", data: {} })
-    }
-}
-)
-
-productsRouter.get("*", (req, res, next) => {
-    res.status(404).json({ status: "error", msg: "Route not found", data: {} })
-})
-
